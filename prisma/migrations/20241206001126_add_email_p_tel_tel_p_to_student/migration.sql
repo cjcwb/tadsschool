0698d1dddc8b39e5b81cc94eca39a7971d5b3f18
Lucas Dias <lucasdias789@gmail.com>
2024-12-09 15:20:18 -0300

    fix repo

--- prisma/migrations/20241206001126_add_email_p_tel_tel_p_to_student/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Student" ADD COLUMN "emailP" TEXT;
-ALTER TABLE "Student" ADD COLUMN "tel" TEXT;
-ALTER TABLE "Student" ADD COLUMN "telP" TEXT;
--- src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date) => {
-    if (!date) return "";
-    
-    const options = { day: "2-digit", month: "2-digit", year: "numeric" };
-    return new Intl.DateTimeFormat("pt-BR", options).format(new Date(date));
-  };
-  
